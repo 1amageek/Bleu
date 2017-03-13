4d8976b6e8159a1b0eca6074617447c972fab6f8
1_am_a_geek <tmy0x3@icloud.com>
2017-03-13 10:47:36 +0900

    Initial Commit

---
+++ Bleu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bleu
+//
+//  Created by 1amageek on 2017/03/13.
+//  Copyright © 2017年 Stamp inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
